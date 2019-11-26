cb050f91756b9b2529c3c29ffd608f550f1090aa
UCSLP <58234381+UCSLP@users.noreply.github.com>
2019-11-26 15:37:02 -0600

    Update server.js

--- server.js
+++ server.js
@@ -32,16 +32,16 @@ http.createServer(function (req, res) {
             var dy = jugadores[i].pos[1] - jugador.pos[1];
             var d = Math.sqrt((dx*dx)+(dy*dy));
             var objetojugador
-            if(jugadores[i].color)
+           /* if(jugadores[i].color)
             {
               objetojugador = {nombre: jugadores[i].nom, Esta_a: d + " Unidades", mide: jugadores[i].tam, color: jugadores[i].color};
             }
-            else objetojugador = {nombre: jugadores[i].nom, Esta_a: d + " Unidades", mide: jugadores[i].tam};
+            else */objetojugador = {nombre: jugadores[i].nom, Esta_a: d + " Unidades", mide: jugadores[i].tam};
             otrosjugadores.push(objetojugador);
           }
           res.end(JSON.stringify(otrosjugadores));
         }
-        if(typeof jugador.col === 'object')
+        /*if(typeof jugador.col === 'object')
         {
           for(var i = 0; i < jugadores.length; i++){
             if(jugador.nom === jugadores[i].nom)
@@ -49,7 +49,7 @@ http.createServer(function (req, res) {
               jugadores[i].color = jugador.col;
             }
           }
-        }
+        }*/
     } else {
       res.end('incorrecto');
     }
